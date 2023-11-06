0ba5545f9cce171fa2ed780c58a7a0527468ef67
guoyaol <guoyaoli00@gmail.com>
2023-11-06 11:42:41 -0500

    add new models in init

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,6 @@ from .llama_2_7b_chat_hf_q4f16_0 import *
 from .llama_2_7b_chat_hf_q4f16_1 import *
 from .llama_2_13b_chat_hf_q4f16_1 import *
 from .llama_2_13b_chat_hf_q4f16_0 import *
+from .batch16_llama_2_7b_chat_hf_q4f16_1 import *
+from .batch16_llama_2_13b_chat_hf_q4f16_1 import *
+from .stable_diffusion_xl import *
